cd89da9eddc242bb54e43a2445dd0508e81385ab
admin <admin@tplwkpcente-a14>
2018-06-28 11:19:48 +0200

    Elvis- Crear Conexion a BBDD, subir jar de mysql

--- src/utilidades/ConexionBBDD.java
+++ src/utilidades/ConexionBBDD.java
@@ -1,9 +1,31 @@
 package utilidades;
 
-public interface ConexionBBDD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	String URL = "jdbc:mysql://localhost:3306/tuMovilBarato";
-    String USER = "root";
-    String PASSWORD = "1111";
+public class ConexionBBDD {
+
 	
+	static String URL = "jdbc:mysql://localhost:3306/tuMovilBarato";
+    static String USER = "root";
+    static String PASSWORD = "1111";
+	
+    public static void Conexion () { 
+    
+    
+    Connection connection = null;
+   
+    
+    
+    
+    try {
+		connection = DriverManager.getConnection(URL,USER, PASSWORD);
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 }
