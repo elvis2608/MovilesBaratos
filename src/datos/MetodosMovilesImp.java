e931bf9e054edacbded4b924b861be705f73620e
admin <admin@tplwkpcente-a1>
2018-06-28 15:28:30 +0200

    RUBEN- metodo listarMovilesMarca

--- src/datos/MetodosMovilesImp.java
+++ src/datos/MetodosMovilesImp.java
@@ -16,7 +16,7 @@ public class MetodosMovilesImp implements IMetodosMoviles {
 	Connection connection = null;
 
 	@Override
-	public ArrayList<String> ListarMarca() {
+	public ArrayList<String> listarMarca() {
 		// TODO Auto-generated method stub
 		ConexionBBDD c = new ConexionBBDD();
 		ArrayList<String> listaMarcas = new ArrayList<>();
@@ -42,13 +42,39 @@ public class MetodosMovilesImp implements IMetodosMoviles {
 	}
 
 	@Override
-	public ArrayList<Movil> ListarMovilMarca(int idMarca) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Movil> listarMovilMarca(int idMarca) {
+		ConexionBBDD c = new ConexionBBDD();
+		ArrayList<Movil> listaMoviles = new ArrayList<Movil>();
+		connection = c.Conexion();
+
+		try {
+			Statement st = connection.createStatement();
+			rs = st.executeQuery("SELECT * FROM movil INNER JOIN marca ON movil.idMovil = marca.idMovil WHERE movil.idMarca=="+idMarca+"");
+
+			while (rs.next()) {
+				Movil movil= new Movil();
+				movil.setIdMovil( rs.getInt(1));
+				movil.setNombre( rs.getString(2));
+				movil.setProcesador( rs.getString(3));
+				movil.setMemoria( rs.getInt(4));
+				movil.setBateria( rs.getInt(5));
+				movil.setFoto( rs.getString(6));
+				movil.setPrecio( rs.getInt(7));
+				movil.setStock(rs.getInt(8));
+				
+				listaMoviles.add(movil);
+				//System.out.println(nombre);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listaMoviles;
 	}
 
 	@Override
-	public ArrayList<Movil> ListarMoviles() {
+	public ArrayList<Movil> listarMoviles() {
 		// TODO Auto-generated method stub
 		return null;
 	}
