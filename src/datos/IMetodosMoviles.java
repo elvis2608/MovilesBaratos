e11b9c8764a3d9ae36158195291869c6a51c9f80
admin <admin@tplwkpcente-a14>
2018-06-28 11:37:34 +0200

    Elvis- creado interfaz de datos y su implementacion creado carpeta de css

---
+++ src/datos/IMetodosMoviles.java
@@ -0,0 +1,6 @@
+package datos;
+
+public interface IMetodosMoviles {
+
+	public void ListarMarca ();
+}
---
+++ src/datos/MetodosMovilesImp.java
@@ -0,0 +1,13 @@
+package datos;
+
+public class MetodosMovilesImp implements IMetodosMoviles {
+
+	@Override
+	public void ListarMarca() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	
+}
