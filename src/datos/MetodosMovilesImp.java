b2567495ffd9e38d851a781f2e06eb7b15d4234d
admin <admin@tplwkpcente-a14>
2018-06-28 15:36:31 +0200

    Elvis - Creado metodos listarMoviles     Gamero - BuscarMovil por Id

--- src/datos/MetodosMovilesImp.java
+++ src/datos/MetodosMovilesImp.java
@@ -50,13 +50,74 @@ public class MetodosMovilesImp implements IMetodosMoviles {
 	@Override
 	public ArrayList<Movil> ListarMoviles() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		ConexionBBDD c = new ConexionBBDD();
+		ArrayList<Movil> listaMoviles = new ArrayList<>();
+		connection = c.Conexion();
+		
+		try {
+			Statement st = connection.createStatement();
+			rs = st.executeQuery("SELECT * FROM movil INNER JOIN marca ON movil.idMarca = marca.idMarca");
+			
+					while (rs.next()) {
+						/*
+						Movil movil = new Movil(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6), rs.getString(7), rs.getInt(8), rs.getInt(9));
+						listaMoviles.add(movil);
+						*/
+						Movil movil = new Movil();
+						movil.setIdMovil(rs.getInt(1));
+						movil.setNombre(rs.getString(2));
+						movil.setProcesador(rs.getString(3));
+						movil.setMemoria(rs.getInt(4));
+						movil.setBateria(rs.getInt(5));
+						movil.setFoto(rs.getString(6));
+						movil.setStock(rs.getInt(7));
+						listaMoviles.add(movil);
+					}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return listaMoviles;
 	}
 
 	@Override
 	public Movil bucarMovil(int idMovil) {
 		// TODO Auto-generated method stub
-		return null;
+		ConexionBBDD c = new ConexionBBDD();
+		Movil movil = new Movil();
+		connection = c.Conexion();
+		
+		try {
+			Statement st = connection.createStatement();
+			rs = st.executeQuery("SELECT * FROM movil WHERE movil.idMovil=="+idMovil+"");
+			
+						/*
+						Movil movil = new Movil(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6), rs.getString(7), rs.getInt(8), rs.getInt(9));
+						listaMoviles.add(movil);
+						*/
+						// Asegurarse que solo llega un móvil
+						movil.setIdMovil(rs.getInt(1));
+						movil.setNombre(rs.getString(2));
+						movil.setProcesador(rs.getString(3));
+						movil.setMemoria(rs.getInt(4));
+						movil.setBateria(rs.getInt(5));
+						movil.setFoto(rs.getString(6));
+						movil.setStock(rs.getInt(7));
+						 
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return movil;
 	}
 
 }
