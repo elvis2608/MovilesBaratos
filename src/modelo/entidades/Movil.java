cd78dda8088a443346ac1409afe5f0ff038ceb0e
admin <admin@tplwkpcente-a2>
2018-06-28 12:17:27 +0200

    Gamero: Creado JavaDoc móvil

--- src/modelo/entidades/Movil.java
+++ src/modelo/entidades/Movil.java
@@ -1,22 +1,86 @@
 package modelo.entidades;
 
+/**
+ * Clase Movil
+ *
+ * Contiene toda la información del móvil tanto información técnica del producto
+ * como la información sobre el objeto en web.
+ *
+ * @author Gamero
+ * @version 1.0
+ */
 public class Movil {
+	// Atributos
 
+	/**
+	 * Código del producto
+	 */
 	private int idMovil;
+	/**
+	 * Nombre de la marca comercial
+	 */
 	private String nombre;
+	/**
+	 * Módelo del móvil
+	 */
 	private String marca;
+	/**
+	 * Nombre del procesador
+	 */
 	private String procesador;
+	/**
+	 * Memoria interna del teléfono en Gigabytes
+	 */
 	private int memoria;
+	/**
+	 * Capacidad de la bateria en miliamperios/hora
+	 */
 	private int bateria;
+	/**
+	 * Nombre de la foto en el sistema
+	 */
 	private String foto;
+	/**
+	 * Precio en Euros para el cliente (sin gastos de envío)
+	 */
 	private int precio;
+	/**
+	 * Número de móviles disponibles para venta web
+	 */
 	private int stock;
-	
+
+	// Constructores
+
+	/**
+	 * Constructor por defecto
+	 */
 	public Movil() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Constructor con 9 parametros
+	 * 
+	 * @param idMovil
+	 *            Código del producto
+	 * @param nombre
+	 *            Nombre de la marca
+	 * @param marca
+	 *            Modelo del móvil
+	 * @param procesador
+	 *            Nombre comercial procesador
+	 * @param memoria
+	 *            Memoria interna móvil
+	 * @param bateria
+	 *            Capacidad de la batería
+	 * @param foto
+	 *            Nombre de la foto del móvil
+	 * @param precio
+	 *            Valor monetario del móvil
+	 * @param stock
+	 *            Móviles disponibles
+	 */
 	public Movil(int idMovil, String nombre, String marca, String procesador, int memoria, int bateria, String foto,
 			int precio, int stock) {
 		super();
@@ -31,9 +95,24 @@ public class Movil {
 		this.stock = stock;
 	}
 
+	// Getters y Setters
+
+	/**
+	 * Devuelve el id del teléfono
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>id del teléfono</li>
+	 *         </ul>
+	 */
+
 	public int getIdMovil() {
 		return idMovil;
 	}
+
+	/**
+	 * asigna al móvil un id
+	 */
 
 	public void setIdMovil(int idMovil) {
 		this.idMovil = idMovil;
@@ -102,6 +181,7 @@ public class Movil {
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
+	// Otros métodos públicos
 
 	@Override
 	public String toString() {
@@ -109,9 +189,5 @@ public class Movil {
 				+ ", memoria=" + memoria + ", bateria=" + bateria + ", foto=" + foto + ", precio=" + precio + ", stock="
 				+ stock + "]";
 	}
-	
-	
-	
-	
-	
+
 }
