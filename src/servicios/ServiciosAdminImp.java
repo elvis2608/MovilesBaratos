0570f476fe34eaaef362406102a49d8d3f123ee9
admin <admin@tplwkpcente-a14>
2018-07-02 14:49:27 +0200

    Elvis - Servicios Admin Impl

--- src/servicios/ServiciosAdminImp.java
+++ src/servicios/ServiciosAdminImp.java
@@ -1,25 +1,33 @@
 package servicios;
 
+import datos.AccionesAdminImp;
+import datos.IAccionesAdmin;
 import modelo.entidades.Movil;
 
 public class ServiciosAdminImp implements IServiciosAdmin {
 
+	private IAccionesAdmin datos = new AccionesAdminImp();
+	
+	
 	@Override
 	public void addMovil(Movil movil) {
 		// TODO Auto-generated method stub
 		
+		datos.addMovil(movil);
 	}
 
 	@Override
 	public void deleteMovil(int idMovil) {
 		// TODO Auto-generated method stub
 		
+		datos.deleteMovil(idMovil);
 	}
 
 	@Override
 	public void updateMovil(Movil movil) {
 		// TODO Auto-generated method stub
 		
+		datos.updateMovil(movil);
 	}
 	
 
